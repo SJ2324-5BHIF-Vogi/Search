d7c9dfa9cef75b23e420ed891865eeb4c3689d87
chm20241 <chm20241@spengergasse.at>
2023-10-06 12:36:31 +0200

    Models (User & Content) Hinzugefügt

---
+++ Spg.Search/src/Spg.Search.DomainModel/Model/Content.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Search.DomainModel.Model
+{
+    internal class Content
+    {
+        public int Id { get; set; }
+        public int fk_content_id { get; set; }
+    }
+}
---
+++ Spg.Search/src/Spg.Search.DomainModel/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Search.DomainModel.Model
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public int fk_user_id { get; set; }
+    }
+}
