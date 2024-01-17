7fd827c6bc2d5934fae247f22f17f39dc4357468
Marvin <Marvin.kantusch@outlook.com>
2024-01-17 23:09:51 +0100

    Create SearchHistory.cs

---
+++ Spg.Search/src/Spg.Search.DomainModel/Model/SearchHistory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Search.DomainModel.Model
+{
+    public class SearchHistory
+    {
+        public int Id { get; init; }
+        public Guid guid { get; init; } = Guid.NewGuid();
+        public string searchUsername  { get; set; }
+        public string searchContent { get; set; }
+        public SearchHistory()
+        {
+        }
+
+        public SearchHistory(int id, Guid guid, string searchUsername, string searchContent)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException("ID cannot be negative", nameof(id));
+            }
+
+            Id = id;
+            this.guid = guid;
+            this.searchUsername = searchUsername;
+            this.searchContent = searchContent;
+        }
+    }
+}
