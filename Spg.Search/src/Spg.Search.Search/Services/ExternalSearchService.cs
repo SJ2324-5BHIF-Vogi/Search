dfe0813c8b761830f2723949d2807b053a97e9ee
chm20241 <chm20241@spengergasse.at>
2024-01-17 23:26:45 +0100

    ExternalService

---
+++ Spg.Search/src/Spg.Search.Search/Services/ExternalSearchService.cs
@@ -0,0 +1,23 @@
+﻿using Spg.Search.DomainModel.Dtos;
+using Spg.Search.Repository;
+
+namespace Spg.Search.Search.Services
+{
+    public class ExternalSearchService : IExternalSearchService
+    {
+        private readonly IExternalDatabaseRepository _externalDatabaseRepository;
+
+        public ExternalSearchService(IExternalDatabaseRepository externalDatabaseRepository)
+        {
+            _externalDatabaseRepository = externalDatabaseRepository;
+        }
+
+        public async Task<List<SearchResultDto>> SearchExternalAsync(string query, string filter, string sort)
+        {
+            var vogies = await _externalDatabaseRepository.SearchResultsAsync(query, filter, sort);
+
+            // Transformiere die Daten bei Bedarf und gib die Ergebnisse zurück
+            return vogies?.Select(vogies => new SearchResultDto { searchContent = vogies.searchContent }).ToList();
+        }
+    }
+}
