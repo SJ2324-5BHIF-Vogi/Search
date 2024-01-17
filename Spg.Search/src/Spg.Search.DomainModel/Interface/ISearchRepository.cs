461c43d632061eab67ea63eff39224458c41c0fb
Raymond <robiermler3@gmail.com>
2024-01-17 23:14:30 +0100

    create search interfaces

---
+++ Spg.Search/src/Spg.Search.DomainModel/Interface/ISearchRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Spg.Search.DomainModel;
+using Spg.Search.DomainModel.Dtos;
+using Spg.Search.DomainModel.Model;
+
+public interface ISearchRepository
+{
+    IEnumerable<SearchHistoryDto> GetAllSearchHistory();
+    SearchHistoryDto GetSearchHistoryById(int id);
+    //GetSearchHistoryByUsername
+    IEnumerable<SearchHistoryDto> GetSearchHistoryByUsername(string username);
+    //GetSearchHistoryByContent
+    IEnumerable<SearchHistoryDto> GetSearchHistoryByContent(string content);
+    SearchHistoryDto CreateSearchHistory(SearchHistoryDto searchHistoryDto);
+    SearchHistoryDto UpdateSearchHistory(SearchHistoryDto searchHistoryDto);
+    SearchHistoryDto DeleteSearchHistory(int id);
+}
---
+++ Spg.Search/src/Spg.Search.DomainModel/Interface/ISearchService.cs
@@ -0,0 +1,17 @@
+﻿using Spg.Search.DomainModel.Dtos;
+
+namespace Spg.Search.Search.Services
+{
+    public interface ISearchService
+    {
+        IEnumerable<SearchHistoryDto> GetAllSearchHistory();
+        SearchHistoryDto GetSearchHistoryById(int id);
+        //GetbyUsername und gibt alle einträge mit dem username zurück
+        IEnumerable<SearchHistoryDto> GetSearchHistoryByUsername(string username);
+        //GetbyContent und gibt alle einträge mit dem username zurück
+        IEnumerable<SearchHistoryDto> GetSearchHistoryByContent(string content);
+        SearchHistoryDto CreateSearchHistory(SearchHistoryDto searchHistoryDto);
+        SearchHistoryDto UpdateSearchHistory(SearchHistoryDto searchHistoryDto);
+        SearchHistoryDto DeleteSearchHistory(int id);
+    }
+}
