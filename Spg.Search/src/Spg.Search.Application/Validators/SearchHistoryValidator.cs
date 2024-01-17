111e805634f8589de93dec16dc553e5512c8450a
Raymond <robiermler3@gmail.com>
2024-01-17 23:21:09 +0100

    Create SearchHistoryValidator.cs

---
+++ Spg.Search/src/Spg.Search.Application/Validators/SearchHistoryValidator.cs
@@ -0,0 +1,35 @@
+﻿using Spg.Search.DomainModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Search.Application.Validators
+{
+    public class SearchHistoryValidator
+    {
+        public bool Validate(SearchHistory searchHistory)
+        {
+            if (searchHistory == null)
+            {
+                Console.WriteLine("SearchHistory is null.");
+                return false;
+            }
+
+            if (searchHistory.searchUsername == null)
+            {
+                Console.WriteLine("SearchHistory.searchUsername is null.");
+                return false;
+            }
+
+            if (searchHistory.searchContent == null)
+            {
+                Console.WriteLine("SearchHistory.searchContent is null.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
