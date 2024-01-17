5724f0d28ab3ee24679e8b00aceb25f3660ebea7
Marvin <Marvin.kantusch@outlook.com>
2024-01-17 23:18:43 +0100

    Create ExternalDatabaseRepository.cs

---
+++ Spg.Search/src/Spg.Search.Repository/ExternalDatabaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Spg.Search.DomainModel.Dtos;
+
+namespace Spg.Search.Repository
+{
+    public class ExternalDatabaseRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExternalDatabaseRepository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public  async Task<List<SearchResultDto>> SearchResultsAsync(string query, string filter, string sort)
+        {
+           var endpoint = $"https://localhost:44305/api/search?query={query}&filter={filter}&sort={sort}";
+
+           var response = await _httpClient.GetAsync(endpoint);
+
+           if(response.IsSuccessStatusCode)
+            {
+               var content = await response.Content.ReadAsStringAsync();
+               var searchResults = System.Text.Json.JsonSerializer.Deserialize<List<SearchResultDto>>(content);
+               return searchResults;
+           }
+           else
+           {
+               throw new Exception("Unable to retrieve search results");
+           }
+        }   
+    }
+}
