cf3ee4aaac072477277ba4f8ff136d914e50ed65
Raymond <robiermler3@gmail.com>
2024-01-17 23:27:43 +0100

    create services

---
+++ Spg.Search/src/Spg.Search.Search/Services/MongoDBServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Spg.Search.DomainModel.Dtos;
+using Spg.Search.DomainModel.Model;
+using Spg.Search.Search.Models;
+
+namespace Spg.Search.Search.Services
+{
+    public class MongoDBServices : IMongoDBServices
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDBServices(IOptions<SearchDatabaseSettings> options)
+        {
+            //ConnectionURI und DatabaseName und CollectionName aus appsettings.json
+            var client = new MongoClient(options.Value.ConnectionURI);
+            _database = client.GetDatabase(options.Value.DatabaseName);
+
+
+        }
+
+        public IMongoDatabase GetDatabase()
+        {
+            return _database;
+        }
+    }
+}
---
+++ Spg.Search/src/Spg.Search.Search/Services/SearchService.cs
@@ -0,0 +1,51 @@
+﻿using Spg.Search.DomainModel.Dtos;
+
+namespace Spg.Search.Search.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly ISearchRepository _searchRepository;
+
+        public SearchService(ISearchRepository searchRepository)
+        {
+            _searchRepository = searchRepository ?? throw new ArgumentNullException(nameof(searchRepository));
+        }
+
+        public IEnumerable<SearchHistoryDto> GetAllSearchHistory()
+        {
+            return _searchRepository.GetAllSearchHistory();
+        }
+
+        public SearchHistoryDto GetSearchHistoryById(int id)
+        {
+            return _searchRepository.GetSearchHistoryById(id);
+        }
+
+        //SearchHistoryByUsername und gibt alle einträge mit dem username zurück
+        public IEnumerable<SearchHistoryDto> GetSearchHistoryByUsername(string username)
+        {
+            return _searchRepository.GetAllSearchHistory().Where(x => x.searchUsername == username);
+        }
+
+        //SearchHistoryByContent
+        public IEnumerable<SearchHistoryDto> GetSearchHistoryByContent(string content)
+        {
+            return _searchRepository.GetAllSearchHistory().Where(x => x.searchContent == content);
+        }
+
+        public SearchHistoryDto CreateSearchHistory(SearchHistoryDto searchHistoryDto)
+        {
+            return _searchRepository.CreateSearchHistory(searchHistoryDto);
+        }
+
+        public SearchHistoryDto UpdateSearchHistory(SearchHistoryDto searchHistoryDto)
+        {
+            return _searchRepository.UpdateSearchHistory(searchHistoryDto);
+        }
+
+        public SearchHistoryDto DeleteSearchHistory(int id)
+        {
+            return _searchRepository.DeleteSearchHistory(id);
+        }
+    }
+}
