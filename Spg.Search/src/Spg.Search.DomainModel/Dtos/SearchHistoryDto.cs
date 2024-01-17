d84a988d1655a86e129b678130e96263d83cc5b5
Marvin <Marvin.kantusch@outlook.com>
2024-01-17 23:11:14 +0100

    Create Dtos

---
+++ Spg.Search/src/Spg.Search.DomainModel/Dtos/SearchHistoryDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Search.DomainModel.Dtos
+{
+    public record SearchHistoryDto
+    {
+        //SearchHistoryDto Properties
+        public int Id { get; set; }
+        public Guid guid { get; set; } = Guid.NewGuid();
+        public string searchUsername { get; set; }
+        public string searchContent { get; set;}
+    }
+}
---
+++ Spg.Search/src/Spg.Search.DomainModel/Dtos/SearchResultDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Search.DomainModel.Dtos
+{
+    public class SearchResultDto
+    {
+        public int Id { get; set; }
+        public string searchUsername { get; set; }
+        public string searchContent { get; set; }
+    }
+}
