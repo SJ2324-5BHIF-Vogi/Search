c9f4c2c16b3e51b2cfc6b2504fa6f74d34ab608d
Raymond <robiermler3@gmail.com>
2024-01-17 23:15:56 +0100

    create context

---
+++ Spg.Search/src/Spg.Search.Infrastructure/YourDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Spg.Search.DomainModel.Model;
+
+namespace Spg.Search.Infrastructure;
+public class YourDbContext : DbContext
+{
+    public YourDbContext(DbContextOptions<YourDbContext> options) : base(options)
+    {
+    }
+
+    protected YourDbContext()
+        : this(new DbContextOptions<YourDbContext>())
+    { }
+
+    public DbSet<SearchHistory> SearchHistories { get; set; } = null!;
+
+
+}
